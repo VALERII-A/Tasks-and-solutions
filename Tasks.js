2cdc48cd5e0408028902f63ed85e013a683e478d
VALERII-A <saligasaliga100@gmail.com>
2023-03-23 16:57:32 +0500

    C:/Program Files/Git/

--- Tasks.js
+++ Tasks.js
@@ -1,1 +1,9 @@
-!
+const hero = {
+    heroName: 'Герой #1',
+    sayNormal(message) {
+      console.log(`${this.heroName}: ${message}`);
+    },
+    sayLouder(message) {
+      console.log(`${this.heroName}: ${message.toUpperCase()}`);
+    }
+  };
