aa219ed11f0d369de2633eab9e7d6567809e3861
VALERII-A <saligasaliga100@gmail.com>
2023-04-09 20:10:08 +0500

    +

--- Tasks.js
+++ Tasks.js
@@ -1,1 +1,8 @@
-.
+let values = ["100", "30", "Не число", "20", "Тоже не число"];
+let x = 0;
+const total = values.reduce((sum, elem) => {
+if (isNaN(elem) !== true) {
+x += parseFloat(elem)
+}
+},0)
+console.log(x)
