86988a51aee7462e6b830076fa75328a105a9df2
VALERII-A <saligasaliga100@gmail.com>
2023-05-22 12:46:16 +0500

    5+

--- Tasks.ts
+++ Tasks.ts
@@ -64,9 +64,13 @@ function filterAnagrams(word: string, array: string[]): string[] {
 // 5 //
 // Реализуйте функцию isComplete(), которая принимает на вход курс и определяет, завершен ли он. 
 // Завершенным считается курс, в который добавлено четыре или более уроков:
-// Пример:
+//  Пример:
 // const course = {
 //   name: 'Java',
 //   lessons: ['variables', 'functions', 'conditions'],
 // };
 // isComplete(course); false
+//  Решение:
+function isComplete(course: { name: string, lessons: string[] }): boolean {
+  return course.lessons.length >= 4;
+}
