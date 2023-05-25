901a96863065403d63b4ccf961ebeae34ec3a283
VALERII-A <saligasaliga100@gmail.com>
2023-05-25 11:16:58 +0500

    7+

--- Tasks.ts
+++ Tasks.ts
@@ -98,3 +98,18 @@ const buildModal = (text: string, status: ModalStatus): { text: string; status: 
 // 7 //
 // Реализуйте функцию getOlderUser(), которая принимает на вход двух пользователей и возвращает того, который старше. 
 // Если пользователи являются ровесниками, то возвращается null:
+//  Решение:
+type User = {
+  name: string,
+  age: number,
+}
+
+function getOlderUser(user1: User, user2: User): User | null {
+  if (user1.age > user2.age) {
+    return user1
+  }
+  if (user2.age > user1.age) {
+    return user2
+  }
+  return null
+}
