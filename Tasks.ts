be898d94623ecf7546019f9cfc634ca0f393bf48
VALERII-A <saligasaliga100@gmail.com>
2023-05-29 11:21:58 +0500

    8

--- Tasks.ts
+++ Tasks.ts
@@ -113,3 +113,12 @@ function getOlderUser(user1: User, user2: User): User | null {
   }
   return null
 }
+
+
+// 8 //
+// Реализуйте функцию getParams(), которая принимает на вход строку запроса (query string) и возвращает параметры в виде объекта:
+//  Пример:
+// getParams('per=10&page=5');
+// // { per: '10', page: '5' }
+// getParams('name=hexlet&count=3&order=asc');
+// // { name: 'hexlet', count: '3', order: 'asc' }
