f41b4e59117a684530031f071cda47de2b384d3e
VALERII-A <saligasaliga100@gmail.com>
2023-05-30 14:48:54 +0500

    9

--- Tasks.ts
+++ Tasks.ts
@@ -134,3 +134,13 @@ function getParams(string:string) {
    return result
  }
  
+
+// 9 //
+//  Реализуйте namespace Company, в котором экспортируется функция isEmployeeEmail(). 
+//  Функция принимает почту и домен. Если емейл пользователя содержит указанный домен, то функция возвращает true:
+//  Пример:
+// Company.isEmployeeEmail('tirion@hexlet.io', 'hexlet.io');
+// true
+// Company.isEmployeeEmail('user@example.com', 'hexlet.io');
+// false
+//  Решение:
