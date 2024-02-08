fe8080cba120d638a309acdafc4149049834b88f
VALERII-A <saligasaliga100@gmail.com>
2024-02-08 16:45:01 +0500

    6

--- Tasks.js
+++ Tasks.js
@@ -5,8 +5,8 @@
 // Пример:
 // divisibleBy([1, 2, 3, 4, 5, 6], 2) == [2, 4, 6]
 // Решение:
-function divisibleBy (numbers, divisor) {
-    return numbers.filter(e => e % divisor === 0)
+function divisibleBy(numbers, divisor) {
+  return numbers.filter((e) => e % divisor === 0);
 }
 
 // 2 //
@@ -16,23 +16,30 @@ function divisibleBy (numbers, divisor) {
 // Подсчитайте, сколько лет назад отец был вдвое старше сына (или через сколько лет он будет вдвое старше).
 //  Решение:
 function twiceAsOld(dadYearsOld, sonYearsOld) {
-    return  Math.abs(dadYearsOld - (sonYearsOld * 2)) 
-    }
+  return Math.abs(dadYearsOld - sonYearsOld * 2);
+}
 
 // 3 //
-// Если задано число n, вернуть количество положительных нечетных чисел меньше n 
+// Если задано число n, вернуть количество положительных нечетных чисел меньше n
 //  Решение:
-const oddCount = n => (n%2 !== 0 ? (n - 1) / 2 : n/2 )
+const oddCount = (n) => (n % 2 !== 0 ? (n - 1) / 2 : n / 2);
 
 // 4 //
-// Преобразуйте строку в число 
+// Преобразуйте строку в число
 //  Решение:
-const stringToNumber = str => (+str)
+const stringToNumber = (str) => +str;
 
 // 5 //
 // Ваша задача - отсортировать заданную строку. Каждое слово в строке будет содержать одно число.
 // Это число - позиция, которую слово должно занимать в результате.
 // Решение:
 function order(words) {
-    return words.split(' ').sort((a, b) => a.match(/\d/) - b.match(/\d/)).join(' ');
-  }
+  return words
+    .split(" ")
+    .sort((a, b) => a.match(/\d/) - b.match(/\d/))
+    .join(" ");
+}
+// 6 //
+//  Определить сколько раз каждый элемент встречается в массиве.
+//  Пример:
+//  const fruits = ['kiwi', 'apple', 'kiwi', 'orange', 'kiwi', 'apple'];
